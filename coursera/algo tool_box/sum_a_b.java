f34325974523fe3eacd55286bb2f36d85d8313db
sachin-611 <shersingh6112000@gmail.com>
2020-11-24 13:29:01 +0530

    removed coursera algo toolbox

--- coursera/algo tool_box/sum_a_b.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class sum_a_b{
-    final static Scanner sc=new Scanner(System.in);
-    public static void main(String args[]){
-        int a=sc.nextInt();int b=sc.nextInt();
-        System.out.println(a+b);
-    }
-}
